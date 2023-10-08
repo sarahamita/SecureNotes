48512fae93c5cac6bfef18760f1babef8a4292f2
Sarah Amita Dewi <sarahamitadewi@gmail.com>
2023-10-08 10:51:43 +0700

    fix error on jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   preset: 'react-native',
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|react-native-keychain|other-module-to-transform)/)',
+  ],
 };
