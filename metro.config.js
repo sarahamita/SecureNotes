ddac1798b7fd639927f9413c865cb47860174f6e
Sarah Amita Dewi <sarahamitadewi@gmail.com>
2023-10-08 10:51:28 +0700

    handle config

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,11 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    /* Resolver config */
+    sourceExts: ['jsx', 'js', 'ts', 'tsx', 'json'], // Add this line
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
